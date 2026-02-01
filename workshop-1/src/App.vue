5073d710ae7ddc93538724c8be3926ca12762424
Wesal Siddiqi <wesalsiddiqi@Mac.fritz.box>
2026-02-01 19:45:20 +0100

    Initial commit

---
+++ workshop-1/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>{{ amount }}</div>
+  <button
+   style="padding: 13px; background-color: blue;" @click="handleClick"> 
+  
+   Zähl hoch
+    </button>
+
+
+</template>
+
+<script setup>
+let amount = 0; 
+function handleClick() {
+amount++;
+console.log(amount)
+}
+
+</script>
+
+<style scoped>
+
+</style>
